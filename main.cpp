fbf252a862b5e6e23a706b0bb734be61ee37b252
Albyteros <andrewwatters622@gmail.com>
2026-09-18 16:32:16 -0500

    added logic, basic printing

--- main.cpp
+++ main.cpp
@@ -43,7 +43,43 @@ int main( int argc, char * argv[] )
 	loan_amount = arguments[0];
 	yearly_interest_rate = arguments[1];
 	monthly_payment = arguments[2];
-	cout << loan_amount << " " << yearly_interest_rate << " " << monthly_payment << endl;
+	cout << "Loan Amount -> " << loan_amount << endl;
+	cout << "Yearly Interest Rate % -> " << yearly_interest_rate << endl;
+	cout << "Monthly Payment -> " << monthly_payment << endl;
+
+	int month = 1;
+	double monthly_interest_rate = (yearly_interest_rate / 12);
+
+
+	//checking if payment <= interest so program does not run infinitely
+    if (monthly_payment <= loan_amount * (yearly_interest_rate / 1200)){
+    std::cout << "You will never pay off your debt." << std::endl;
+    return 0;
+    }
+
+    double balance = loan_amount;
+    double interest;
+    double total_interest = 0;
+
+	// main loop of printing
+	while (balance > 0){
+	interest = balance * (monthly_interest_rate / 100);
+    balance += interest;
+    balance -= monthly_payment;
+    total_interest += interest;
+    
+	// print block- we may need a better alignment than 5 whitespaces
+    std::cout << month << "     ";
+    std::cout << balance << "     ";
+    std::cout << monthly_payment << "     ";
+    std::cout << monthly_interest_rate << "     ";
+    std::cout << interest << "     ";
+    std::cout << (monthly_payment - interest) << std::endl;
+    month++;
+	}
+
+    std::cout << "It takes " << month << " months to pay off the loan" << std::endl;
+    std::cout << "total interest paid is: " << total_interest;
 
 	return 0;
 }
